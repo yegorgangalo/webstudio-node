8fb547b65f2079366b91814f3b0b90c58d595ae0
Yegor Gangalo <gangaloyegor@gmail.com>
2021-03-06 01:21:43 +0200

    add clients json

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ const typesPath = path.join(__dirname, "./db/portfolio-types.json");
 const mailingPath = path.join(__dirname, "./db/mailing.json");
 const formPath = path.join(__dirname, "./db/form.json");
 const advantagesPath = path.join(__dirname, "./db/advantages.json");
+const clientsPath = path.join(__dirname, "./db/clients.json");
 const PORT = process.env.PORT || 3003;
 
 app.use(express.urlencoded({ extended: true })) //дає можливість зчитувати body в req.body.title// Parse URL-encoded bodies (as sent by HTML forms)
@@ -22,6 +23,16 @@ app.get('/advantages', async (req, res) => {
         const data = await fsProm.readFile(advantagesPath, "utf-8");
         const advantages = JSON.parse(data);
         res.status(200).send(advantages);
+    } catch (err) {
+        return console.error(err.message);
+    }
+})
+
+app.get('/clients', async (req, res) => {
+    try {
+        const data = await fsProm.readFile(clientsPath, "utf-8");
+        const clients = JSON.parse(data);
+        res.status(200).send(clients);
     } catch (err) {
         return console.error(err.message);
     }
